3f12b1cdfed3e2e2e1e04a3ea4d68245284c6b23
Scion-Kin <dinturner17@gmail.com>
2024-03-27 22:45:32 +0200

    Task completed

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' This script returns information about all employees' TODO
+list progress and saves it to a json file '''
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+
+    all_data = response.json()
+
+    all_users = []
+    for i in all_data:
+        if i['userId'] not in all_users:
+            all_users.append(i['userId'])
+
+    with open('todo_all_employees.json', 'w') as file:
+
+        data = {}
+        for i in all_users:
+            employee = requests.get('https://jsonplaceholder.typicode.com/\
+            users/{}'.format(i))
+            employee = employee.json()
+            name = employee['username']
+
+            temp = []
+            for j in all_data:
+                if j['userId'] == i:
+                    user_data = {"username": name,
+                                 "task": j['title'],
+                                 "completed": j['completed']}
+
+                    temp.append(user_data)
+            print('finished handling user {}. Name: {}\n\n'.format(i, name))
+
+            data[i] = temp
+
+        json.dump(data, file)
