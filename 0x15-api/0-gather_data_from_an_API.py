7c0c47373f335e9a69a45b65d83b2db05616326f
Scion-Kin <dinturner17@gmail.com>
2024-03-27 18:13:56 +0200

    Task completed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' This module returns information about his/her TODO 
+''' This module returns information about his/her TODO
 list progress for a given employee ID. '''
 
 import requests
@@ -19,16 +19,18 @@ if __name__ == '__main__' and sys.argv[1]:
         if i['userId'] == id:
             total_tasks = total_tasks + 1
 
-        if i['userId'] == id and i['completed'] == True:
+        if i['userId'] == id and i['completed']:
             count = count + 1
 
             tasks.append(i['title'])
 
-    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(id))
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(id))
     employee = employee.json()
     name = employee['name']
 
-    print('Employee {} is done with tasks({}/{}):'.format(name, count, total_tasks))
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name, count, total_tasks))
 
     for i in tasks:
         print('\t{}'.format(i))
