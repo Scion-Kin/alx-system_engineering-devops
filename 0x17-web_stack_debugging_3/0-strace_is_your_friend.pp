1d64ee58feb4095376549222cc47be77c0e73220
Scion-Kin <dinturner17@gmail.com>
2024-04-11 10:06:13 +0200

    Project complete

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # automated puppet fix for a php configuration error
 
 exec { 'Fix wordpress site':
-  require  => shell,
   command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
