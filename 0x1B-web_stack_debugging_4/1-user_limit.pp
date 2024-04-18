167d17f23b62291e3d8318fbf8979d37c5f4c5e1
Scion-Kin <dinturner17@gmail.com>
2024-04-18 14:01:55 +0200

    User reads escalated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 # The problem was the few read file permission assigned to this user
 
 exec {'escalate':
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 10000/" /etc/security/limits.conf',
-  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 10000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/nofile 5/holberton hard nofile 10000/" /etc/security/limits.conf',
+  command  => 'sed -i "s/nofile 4/holberton hard nofile 10000/" /etc/security/limits.conf',
   provider => shell,
 }
